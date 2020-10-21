f443d9542cd410aa7427e23717c81eed752d359b
Igor Podolsiy <delusionhs@mail.ru>
2020-10-21 21:15:08 +0200

    feat: add weather service

---
+++ KodeWeather/Services/WeatherService.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherService.swift
+//  KodeWeather
+//
+//  Created by Igor Podolskiy on 21.10.2020.
+//
+
+protocol WeatherServiceProtocol {
+}
+
+class  WeatherService: WeatherServiceProtocol {
+}
