eee2a4ad6b84530691ae609d6245e384345828d8
Igor Podolsiy <delusionhs@mail.ru>
2020-10-22 19:23:42 +0200

    feat: Add data loading

--- KodeWeather/Modules/Weather/Presenter/WeatherPresenter.swift
+++ KodeWeather/Modules/Weather/Presenter/WeatherPresenter.swift
@@ -10,10 +10,19 @@ final class WeatherPresenter {
     // MARK: - Properties
 
     weak var view: WeatherViewInput?
+    private let weatherService: WeatherServiceProtocol = WeatherService()
 }
 
 // MARK: - SearchViewOutput
 extension WeatherPresenter: WeatherViewOutput {
     func viewLoaded() {
+        weatherService.fetchWeatherData(country: "") { (result) in
+            switch result {
+            case .success(let data):
+                print(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
--- KodeWeather/Services/WeatherService.swift
+++ KodeWeather/Services/WeatherService.swift
@@ -4,9 +4,39 @@
 //
 //  Created by Igor Podolskiy on 21.10.2020.
 //
+import Foundation
 
 protocol WeatherServiceProtocol {
+    func fetchWeatherData(country: String, completion: @escaping (Result<WeatherResponse?, NetworkError>) -> Void)
 }
 
 final class  WeatherService: WeatherServiceProtocol {
+    private let networkService: NetworkServiceProtocol = NetworkService()
+
+    private func fetchData<T: Decodable>(API: String, parametres: [String: Any] = [:], completion: @escaping (Result<T?, NetworkError>) -> Void) {
+        guard let url = URL(string: API) else { return }
+        networkService.getJSONData(URL: url, parameters: parametres) { result  in
+            switch result {
+            case .success(let data):
+                let summary = try? JSONDecoder().decode(T.self, from: data)
+                completion(.success(summary))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func fetchWeatherData(country: String, completion: @escaping (Result<WeatherResponse?, NetworkError>) -> Void) {
+        let parametres: [String: Any]
+        parametres = ["lat":33.441792,"lon":-94.037689,"exclude":"current,minutely,daily,alerts",
+                      "lang":"ru","units":"metric","appid":"2db3c57ff9b75e40ba734e02ba73aa25"]
+        fetchData(API:  ApiURL.openWeather, parametres: parametres, completion: completion)
+    }
+}
+
+// MARK: - Constants
+extension WeatherService {
+    private enum ApiURL {
+        static let openWeather = "https://api.openweathermap.org/data/2.5/onecall?"
+    }
 }
