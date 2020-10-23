4a2e60c0537437edb5ab04c82f8b95c701a5a066
Igor Podolsiy <delusionhs@mail.ru>
2020-10-23 03:15:32 +0200

    feat: Add attraction collection view

--- KodeWeather/Modules/Attractions/View/AttractionsViewController.swift
+++ KodeWeather/Modules/Attractions/View/AttractionsViewController.swift
@@ -14,6 +14,14 @@ final class AttractionsViewController: UIViewController {
 
     var output: AttractionsViewOutput?
 
+    let attractionsCollectionView : UICollectionView = {
+        let viewLayout = UICollectionViewFlowLayout()
+        viewLayout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
+        return collectionView
+    }()
+
+
     // MARK: - UIViewController
 
     override func viewDidLoad() {
@@ -25,14 +33,37 @@ final class AttractionsViewController: UIViewController {
     }
 
     private func setupViews() {
+        view.addSubview(attractionsCollectionView)
     }
 
 
     private func setupLayouts() {
+        attractionsCollectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            attractionsCollectionView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor),
+            attractionsCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            attractionsCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            attractionsCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
 //MARK: - AttractionsViewInput
 extension AttractionsViewController: AttractionsViewInput {
+
+}
+
+//MARK: - UICollectionViewDelegate & UICollectionViewDataSource
+
+extension AttractionsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
 
 }
