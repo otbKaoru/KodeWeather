91c407ed7929fd80a7248d8513e86d89699e8766
Igor Podolsiy <delusionhs@mail.ru>
2020-10-23 03:36:59 +0200

    feat: Setup constraints

---
+++ KodeWeather/Modules/Attractions/View/CollectionViewCell/AttractionsCollectionViewCell.swift
@@ -0,0 +1,93 @@
+//
+//  AttractionsCollectionViewCell.swift
+//  KodeWeather
+//
+//  Created by Igor Podolskiy on 23.10.2020.
+//
+
+import UIKit
+
+final class AttractionsCollectionViewCell: UICollectionViewCell {
+
+    private let attractionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+
+    private let attractionTitle: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.adjustsFontSizeToFitWidth = true
+        label.font = UIFont.systemFont(ofSize: CellOptions.attractionTitleFontSize)
+        label.textColor = StyleGuide.Colors.defaultTextColor
+        return label
+    }()
+
+    private let attractionDescription: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.font = UIFont.systemFont(ofSize: CellOptions.attractionDescriptionFontSize)
+        label.textColor = StyleGuide.Colors.defaultTextColor
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupLayouts()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        contentView.layer.cornerRadius = CellOptions.contentViewLayerCornerRadius
+        contentView.addSubview(attractionImageView)
+        contentView.addSubview(attractionTitle)
+        contentView.addSubview(attractionDescription)
+    }
+
+    private func setupLayouts() {
+        attractionImageView.translatesAutoresizingMaskIntoConstraints = false
+        attractionTitle.translatesAutoresizingMaskIntoConstraints = false
+        attractionDescription.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            attractionImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            attractionImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            attractionImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            attractionImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            attractionTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            attractionTitle.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            attractionTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            attractionDescription.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            attractionDescription.topAnchor.constraint(equalTo: attractionTitle.bottomAnchor, constant: LayoutOptions.descriptionTopPadding),
+            attractionDescription.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            attractionDescription.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+
+    }
+}
+
+// MARK: - Constants
+
+extension AttractionsCollectionViewCell {
+    private enum CellOptions {
+        static let attractionTitleFontSize: CGFloat = 18
+        static let attractionDescriptionFontSize: CGFloat = 16
+        static let contentViewLayerCornerRadius: CGFloat = 12
+    }
+
+    private enum LayoutOptions {
+        static let descriptionTopPadding: CGFloat = 16
+    }
+}
---
+++ KodeWeather/Modules/Attractions/View/CollectionViewCell/ttractionsCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  ttractionsCollectionViewCell.swift
+//  KodeWeather
+//
+//  Created by Igor Podolskiy on 23.10.2020.
+//
+
+import Foundation
