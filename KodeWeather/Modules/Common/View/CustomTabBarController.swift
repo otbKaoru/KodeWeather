ed998a04071f8d18dc2e3c9baa2bf795a376c320
Igor <delusionhs@mail.ru>
2020-11-29 01:48:30 +0200

    fix
    fix final

--- KodeWeather/Modules/Common/View/CustomTabBarController.swift
+++ KodeWeather/Modules/Common/View/CustomTabBarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CustomTabBarController: UITabBarController {
+final class CustomTabBarController: UITabBarController {
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .center
